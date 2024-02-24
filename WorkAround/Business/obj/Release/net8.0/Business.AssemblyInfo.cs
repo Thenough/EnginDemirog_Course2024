9c724105706874be79af356a17217b6648c3ae38
Thenough <m.cannn4141@gmail.com>
2024-02-24 12:38:49 +0300

    Give Me Mask

--- WorkAround/Business/obj/Release/net8.0/Business.AssemblyInfo.cs
+++ WorkAround/Business/obj/Release/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb1df9f94a78196d410719ef7a59554488e4eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5afdc30488db6da86ce36506b3f194145960f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WorkAround/Workaround/obj/Release/net8.0/Workaround.AssemblyInfo.cs
+++ WorkAround/Workaround/obj/Release/net8.0/Workaround.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb1df9f94a78196d410719ef7a59554488e4eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5afdc30488db6da86ce36506b3f194145960f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
