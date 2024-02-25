49274936986adb8b302cabd9e2d070b766ad9c86
Thenough <m.cannn4141@gmail.com>
2024-02-25 23:03:40 +0300

    3. Ders WebAPI

---
+++ WebAPI/Core/Entitiy/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity
+{
+    public class BaseEntity<TId>
+    {
+        public TId Id { get; set; }
+        public DateTime CreatedData { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateBrandRequest createBrandRequest)
+        {
+           CreatedBrandResponse createdBrandResponse = _brandService.Add(createBrandRequest);
+
+            //Ok = http 200 (başarılı)
+            //Created = http 2001 (eklendi ve başarılı)
+            return Ok(createdBrandResponse);
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok(_brandService.GetAll());
+        }
+    }
+}
